478b45dfbd50085f88ed6ea1bf7081b6163eb9fc
alininfoiasi <alininfoiasi@gmail.com>
2024-09-23 10:14:26 +0200

    Add lambda and buildspec

---
+++ code/lambda_function.py
@@ -0,0 +1,16 @@
+import json
+import boto3
+import os
+
+print('Loading function v1.0')
+
+client = boto3.client('sns')
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2), end='\n')
+    topicArn = os.environ['SNSARN']
+    print("SNS arn: " + topicArn)
+    for message in event['Records']:
+        response = client.publish(TopicArn=topicArn, Message=message['body'])
+    print("Message published to SNS: " + message['body'])
+    return (response)
---
+++ script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sam deploy --guided --profile user1
